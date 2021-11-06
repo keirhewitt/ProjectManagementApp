5f53fe9678c1f3e9ed72c88a6d058f2530b2bc32
KeirHewitt <57302739+keirhewitt@users.noreply.github.com>
2021-11-06 16:07:11 +0000

    All files

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+Bootstrap(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
